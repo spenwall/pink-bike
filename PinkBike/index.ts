2434777a6f4d48ee2fcdccf2e76062f12d0b08d1
Spencer Wallace <spencer.wallace@outlook.com>
2020-08-30 22:33:41 -0600

    Removed unused functions

--- PinkBike/index.ts
+++ PinkBike/index.ts
@@ -4,47 +4,39 @@ const cheerio = require('cheerio')
 const airtableIds = require('./components/AirtableIds')
 const sendGrid = require('./components/SendGrid')
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
+const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
+    var timeStamp = new Date().toISOString();
+    const url: string = process.env['URL']
 
-    if (req.query.url) {
-        const url = req.query.url
-        const response = await axios.get(req.query.url)
-        const $ = cheerio.load(response.data)
-        const bikes = $('.bsitem')
+    const response = await axios.get(url)
+    const $ = cheerio.load(response.data)
+    const bikes = $('.bsitem')
 
-        try {
-            const oldAds: string[] = await airtableIds.getLastIds(url)
-            const allIds: string[] = []
-            bikes.each((i, ad) => {
-                const adId = $(ad).attr('id')
-                allIds[i] = adId
-                if (oldAds.includes(adId)) {
-                    return
-                }
-                let myAd = {
-                    title: $(ad).find('table:nth-child(1) tbody:nth-child(1) tr:nth-child(1) td:nth-child(2) div:nth-child(1) a:nth-child(1)').text(),
-                    price: $(ad).find('td:nth-child(2) table tr:nth-child(3) b').text(),
-                    location: $(ad).find('td:nth-child(2) table tr:nth-child(1) td').text(),
-                    image: $(ad).find('table:nth-child(1) a img').attr('src'),
-                    link: $(ad).find('td:nth-child(2) a').attr('href')
-                }
+    try {
+        const oldAds: string[] = await airtableIds.getLastIds(url)
+        const allIds: string[] = []
+        bikes.each((i, ad) => {
+            const adId = $(ad).attr('id')
+            allIds[i] = adId
+            if (oldAds.includes(adId)) {
+                return
+            }
+            let myAd = {
+                title: $(ad).find('table:nth-child(1) tbody:nth-child(1) tr:nth-child(1) td:nth-child(2) div:nth-child(1) a:nth-child(1)').text(),
+                price: $(ad).find('td:nth-child(2) table tr:nth-child(3) b').text(),
+                location: $(ad).find('td:nth-child(2) table tr:nth-child(1) td').text(),
+                image: $(ad).find('table:nth-child(1) a img').attr('src'),
+                link: $(ad).find('td:nth-child(2) a').attr('href')
+            }
 
-                sendGrid(myAd)
+            sendGrid(myAd)
 
-            })
-            airtableIds.saveIds(url, allIds)
-        } catch (error) {
-           console.log(error) 
-        }
-
+        })
+        airtableIds.saveIds(url, allIds)
+    } catch (error) {
+        console.log(error) 
     }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a url on the query string or in the request body"
-        };
-    }
+
 };
 
-export default httpTrigger;
+export default timerTrigger;
